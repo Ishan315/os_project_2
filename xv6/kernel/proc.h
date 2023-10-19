e11e563872abe10b9752990df85d759fdbdaf07c
Ishan <patel.ishan.j@gmail.com>
2023-10-19 17:24:44 -0500

    tickets added and initialized

--- xv6/kernel/proc.h
+++ xv6/kernel/proc.h
@@ -74,6 +74,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int tickets;                 // Tickets
 };
 
 // Process memory is laid out contiguously, low addresses first:
